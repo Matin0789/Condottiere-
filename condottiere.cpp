bd5bc07fa458c019e7e13df1d9ba1147447c5811
nimaMakhmali <nmkhmly790@gmail.com>
2024-06-10 21:29:42 +0330

    s

--- condottiere.cpp
+++ condottiere.cpp
@@ -9,12 +9,15 @@ class UserInterface
 class Game
 {
   public:
-Game::Game(UserInterface& inputInterface) : interface(inputInterface){
+Game(UserInterface &inputUI) : cards(92), ui(inputUI), season(nullptr)
+{
     srand(time(0));
-    for (int i = 0;i < 10;i++){
+    for (int i = 0; i < 10; i++)
+    {
         cards[i] = &one_point_yellow_card[i];
     }
-    for (int i = 0;i < 8;i++){
+    for (int i = 0; i < 8; i++)
+    {
         cards[i + 10] = &two_point_yellow_card[i];
         cards[i + 18] = &three_point_yellow_card[i];
         cards[i + 26] = &four_point_yellow_card[i];
@@ -22,29 +25,51 @@ Game::Game(UserInterface& inputInterface) : interface(inputInterface){
         cards[i + 42] = &six_point_yellow_card[i];
         cards[i + 50] = &ten_point_yellow_card[i];
     }
-    for (int i = 0;i < 3;i++){
+    for (int i = 0; i < 3; i++)
+    {
         cards[i + 58] = &turncoat[i];
         cards[i + 61] = &heroine[i];
-        cards[i + 65] = &spring[i];
-        cards[i + 68] = &winter[i];
+        cards[i + 64] = &spring[i];
+        cards[i + 67] = &winter[i];
     }
-    for (int i = 0;i < 16;i++){
-        cards[i + 71] = &scarecrow[i];
+    for (int i = 0; i < 16; i++)
+    {
+        cards[i + 70] = &scarecrow[i];
     }
-    for (int i = 0;i < 12;i++){
-        cards[i + 87] = &spy[i];
+    for (int i = 0; i < 6; i++)
+    {
+
+        cards[i + 86] = &drummer[i];
+        // cards[i + 103] = &bishop[i];
     }
-    for (int i = 0;i < 6;i++){
-        
-        cards[i + 99] = &drummer[i];
-        //cards[i + 105] = &bishop[i];
+    /*for (int i = 0;i < 12;i++){
+        cards[i + 109] = &spy[i];
+    }*/
+}
+
+void distributeCards() {
+    shuffle();
+}
+
+void shuffle()
+{
+    std::random_shuffle(cards.begin(), cards.end());
+}
+
+void play()
+{
+    distributeCards();
+    while (true)
+    {
+        war();
     }
 }
-  	void play()
-    {}
+
+void Game::war() {
+    
+}
   private:  
-  	void shuffle()
-    {}
+
 	UserInterface &interface;
 	GameBoard gameBoard;
 	const std::array<YellowCard, 10> one_point_yellow_card = {YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1),YellowCard(1)};
