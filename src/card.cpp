35e9b820600ac8a033d41e0e964e11f01f7b0a0d
nimaMakhmali <nmkhmly790@gmail.com>
2024-06-10 11:17:54 +0330

    CARD

--- src/card.cpp
+++ src/card.cpp
@@ -2,3 +2,15 @@
 #include "player.h"
 #include "game.h"
 #include "card.h"
+
+unsigned int Card::getPriority() const {
+    return priority ;
+}
+
+unsigned int Card::getPoint() const {
+    return point ;
+}
+
+void Card::setPoint(unsigned int) {
+   this->point = point ; 
+}
