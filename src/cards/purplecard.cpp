de674cca4f78cdb2e185b17497d7df7476c33d96
nimaMakhmali <nmkhmly790@gmail.com>
2024-06-10 10:59:07 +0330

    change

--- src/cards/purplecard.cpp
+++ src/cards/purplecard.cpp
@@ -1,5 +1,5 @@
 #include "purplecard.h"
 
-PurpleCard::PurpleCard(int inputPoint,std::string inputName,int inputPriority,std::string inputHelp) : Card(inputPoint,inputPriority), name(inputName), help(inputHelp) {
+PurpleCard::PurpleCard(int inputPoint,std::string inputName,int inputPriority) : Card(inputPoint,inputPriority) {
 
 }
