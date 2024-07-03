8c83d091a582a70b6fa282936ebf660fc7b4f1b9
matin0789 <matin.a0789@gmail.com>
2024-07-03 22:36:09 +0330

    cmake fixed

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-
 #include "game.h"
 #include "userinterface.h"
 
-int main(int argc, char *argv[])
+int main()
 {
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Condottiere/main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreated,
-        &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
+    UserInterface ui;
+    Game game(ui);
+    game.play();
+    return 0;
 }
