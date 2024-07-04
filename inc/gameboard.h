f5503c365510e05e6d92f9d741ff4c934de89dc4
matin0789 <matin.a0789@gmail.com>
2024-07-04 13:20:20 +0330

    board file added

--- inc/gameboard.h
+++ inc/gameboard.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <map>
 #include <vector>
+#include <list>
 
 #include "state.h"
 
@@ -27,42 +28,13 @@
 class GameBoard
 {
 public:
+	GameBoard(std::string); // (board file path)
 	bool checkAdjacency(const std::string, const std::string, const std::string);
 	State* getState(std::string);
 	std::vector<std::string> get_active_states_name() const;
 private:
-	std::unordered_map<std::string, State> states = {
-		{BEL, State(BEL)},
-		{CAL, State(CAL)},
-		{ENN, State(ENN)},
-		{ATE, State(ATE)},
-		{PLA, State(PLA)},
-		{BOR, State(BOR)},
-		{DIM, State(DIM)},
-		{MOR, State(MOR)},
-		{OLI, State(OLI)},
-		{ROL, State(ROL)},
-		{TAL, State(TAL)},
-		{ARM, State(ARM)},
-		{LIA, State(LIA)},
-		{ELI, State(ELI)}
-	};
-	std::map<std::string,std::map<std::string,bool>> adjacency = {
-		{BEL,	{{CAL, 1}, {ENN, 0}, {ATE, 0}, {PLA, 1}, {BOR, 1}, {DIM, 0}, {MOR, 0}, {OLI, 0}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}}},
-		{CAL,	{{ENN, 1}, {ATE, 1}, {PLA, 1}, {BOR, 1}, {DIM, 0}, {MOR, 0}, {OLI, 0}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 1}}},
-		{ENN,	{{ATE, 1}, {PLA, 0}, {BOR, 1}, {DIM, 1}, {MOR, 0}, {OLI, 0}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 1}, {CAL, 0}}},
-		{ATE,	{{PLA, 0}, {BOR, 0}, {DIM, 1}, {MOR, 0}, {OLI, 0}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 0}, {CAL, 1}, {ENN, 1}}},
-		{PLA,	{{BOR, 1}, {DIM, 0}, {MOR, 1}, {OLI, 0}, {ROL, 1}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 1}, {CAL, 1}, {ENN, 0}, {ATE, 0}}},
-		{BOR,	{{DIM, 1}, {MOR, 1}, {OLI, 1}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 1}, {CAL, 1}, {ENN, 1}, {ATE, 0}, {PLA, 1}}},
-		{DIM,	{{MOR, 1}, {OLI, 1}, {ROL, 0}, {TAL, 0}, {ARM, 0}, {LIA, 0}, {ELI, 0}, {BEL, 0}, {CAL, 0}, {ENN, 1}, {ATE, 1}, {PLA, 0}, {BOR, 1}}},
-		{MOR,	{{OLI, 1}, {ROL, 1}, {TAL, 1}, {ARM, 1}, {LIA, 0}, {ELI, 0}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 1}, {BOR, 1}, {DIM, 0}}},
-		{OLI,	{{ROL, 0}, {TAL, 0}, {ARM, 1}, {LIA, 1}, {ELI, 0}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 0}, {BOR, 1}, {DIM, 1}, {MOR, 1}}},
-		{ROL,	{{TAL, 1}, {ARM, 0}, {LIA, 0}, {ELI, 1}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 1}, {BOR, 0}, {DIM, 0}, {MOR, 1}, {OLI, 0}}},
-		{TAL,	{{ARM, 1}, {LIA, 0}, {ELI, 1}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 0}, {BOR, 0}, {DIM, 0}, {MOR, 1}, {OLI, 0}, {ROL, 1}}},
-		{ARM,	{{LIA, 1}, {ELI, 0}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 0}, {BOR, 0}, {DIM, 0}, {MOR, 1}, {OLI, 1}, {ROL, 0}, {TAL, 1}}},
-		{LIA,	{{ELI, 0}, {BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 0}, {BOR, 0}, {DIM, 0}, {MOR, 0}, {OLI, 1}, {ROL, 0}, {TAL, 0}, {ARM, 1}}},
-		{ELI,	{{BEL, 0}, {CAL, 0}, {ENN, 0}, {ATE, 0}, {PLA, 0}, {BOR, 0}, {DIM, 0}, {MOR, 0}, {OLI, 0}, {ROL, 1}, {TAL, 1}, {ARM, 0}, {LIA, 0}}},
-	};
+	std::unordered_map<std::string, State> states;// states
+	std::map<std::string,std::list<std::string>> adjacency; // list of adjacency
 };
 
 #endif // GAMEBOARD_H
--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -1,11 +1,52 @@
 #include "gameboard.h"
+#include "state.h"
 
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+GameBoard::GameBoard(std::string filePath) {
+    std::ifstream file(filePath);
+    std::string tmp_state_name;
+    while(file >> quoted(tmp_state_name)){ // add state to states and adjacent of them
+        states[tmp_state_name] = State(tmp_state_name);
+        std::string tmp_adjacency_line; //A line in the file where the adjacent is written
+        getline(file, tmp_adjacency_line);// read adjacency line in file
+        std::stringstream tmp(tmp_adjacency_line); // for keep the line of adjacency
+        std::string tmp_adjacency_state_name;// Name of the adjacent state
+        while(tmp >> quoted(tmp_adjacency_state_name)) { // add adjacent of the state to adjacency
+            adjacency[tmp_state_name].push_back(tmp_adjacency_state_name);
+        }
+    }
+}
 
 bool GameBoard::checkAdjacency(const std::string state1Name,const std::string state2Name, const std::string state3Name) {
-    return (adjacency[state1Name][state2Name] && adjacency[state1Name][state3Name] && adjacency[state2Name][state3Name]);
+    // check arguments
+    if (states.find(state1Name) == states.end()){
+        throw std::invalid_argument("State1 not found"); 
+    }
+    if (states.find(state2Name) == states.end()){
+        throw std::invalid_argument("State2 not found"); 
+    }
+    if (states.find(state3Name) == states.end()){
+        throw std::invalid_argument("State3 not found"); 
+    }
+    //
+
+    // check adjacency
+    if(std::find(adjacency[state1Name].begin(), adjacency[state1Name].end(), state2Name) == adjacency[state1Name].end())
+        return false;
+    if(std::find(adjacency[state2Name].begin(), adjacency[state2Name].end(), state3Name) == adjacency[state2Name].end())
+        return false;
+    if(std::find(adjacency[state3Name].begin(), adjacency[state3Name].end(), state1Name) == adjacency[state3Name].end())
+        return false;
+    
+    return true;
+    //
 }
 
 State* GameBoard::getState(std::string name){ 
