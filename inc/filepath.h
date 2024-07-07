85df925bd4040f521eac282a5cfb17c062d6a44a
matin0789 <matin.a0789@gmail.com>
2024-07-07 22:00:40 +0330

    load from file added

---
+++ inc/filepath.h
@@ -0,0 +1,20 @@
+#ifndef FILEPATH_H
+#define FILEPATH_H
+
+// The file path of the game guide
+#define SAVE_FILE(n)        "../data/save/save_" + std::to_string(n) + ".bin"
+
+#define Game_HELP_FILE      "../data/help/GameHelp.txt"
+#define BISHOP_HELP_FILE    "../data/help/BishopHelp.txt"
+#define TURNCOAT_HELP_FILE  "../data/help/TurncoatHelp.txt"
+#define HEROINE_HELP_FILE   "../data/help/HeroineHelp.txt"
+#define SPRING_HELP_FILE    "../data/help/SpringHelp.txt"
+#define WINTER_HELP_FILE    "../data/help/WinterHelp.txt"
+#define SCARECROW_HELP_FILE "../data/help/ScarecrowHelp.txt"
+#define DRUMMER_HELP_FILE   "../data/help/DrummerHelp.txt"
+#define SPY_HELP_FILE       "../data/help/SpyHelp.txt"
+
+#define BOARD_FILE          "../data/board/adjacency.txt"
+
+
+#endif // FILEPATH_H
