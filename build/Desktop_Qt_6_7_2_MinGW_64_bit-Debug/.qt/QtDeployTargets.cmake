b5efa92f5eac39ce373294ab684c1da7e8284518
Matin Amirpanah far <151240953+Matin0789@users.noreply.github.com>
2024-07-07 19:52:43 +0330

    build repo deleted

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Condottiere-_FILE C:/QtProjects/Condottiere/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Condottiere-.exe)
-set(__QT_DEPLOY_TARGET_Condottiere-_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Condottiere-_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
