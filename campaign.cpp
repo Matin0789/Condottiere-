839e0af353aaa15dc27dc1dfbb4375bd4632eb59
nimaMakhmali <nmkhmly790@gmail.com>
2024-08-04 18:14:47 +0330

    debuging and change graphic

--- campaign.cpp
+++ campaign.cpp
@@ -8,27 +8,27 @@ Campaign::Campaign(QWidget *parent)
     , ui(new Ui::Campaign)
 {
     ui->setupUi(this);
-<<<<<<< HEAD
+
    // QLabel *NameLabels = new QLabel(this);
    // for (int var = 0; var < total; ++var)
    // {
 
    // }
-=======
 
-    cards["Bishop"]    = "border-image:url(" + BISHOP_IMAGE    + ")";
-    cards["Drummer"]   = "border-image:url(" + DRUMMER_IMAGE   + ")";
-    cards["Heroine"]   = "border-image:url(" + HEROINE_IMAGE   + ")";
-    cards["Scarecrow"] = "border-image:url(" + SCARECROW_IMAGE + ")";
-    cards["Spring"]    = "border-image:url(" + SPRING_IMAGE    + ")";
-    cards["Winter"]    = "border-image:url(" + WINTER_IMAGE    + ")";
-    cards["Spy"]       = "border-image:url(" + SPY_IMAGE       + ")";
-    cards["Turncoat"]  = "border-image:url(" + TURNCOAT_IMAGE  + ")";
 
-    markers["BattleMarker"] = "border-image:url(" + BATTLE_MARKER_IMAGE  + ")";
-    markers["FavorMarker"]  = "border-image:url(" + PEACE_MARKER_IMAGE   + ")";
-    markers[""]             = "border-image:url(" + PLAYER_MARKER_IMAGE  + ")";
->>>>>>> adb8dafbb721fdbea13bd93ec3fe388d0a621499
+    cards["Bishop"]    = "border-image:url(" + std::string(BISHOP_IMAGE)    + ")";
+    cards["Drummer"]   = "border-image:url(" + std::string(DRUMMER_IMAGE)   + ")";
+    cards["Heroine"]   = "border-image:url(" + std::string(HEROINE_IMAGE)   + ")";
+    cards["Scarecrow"] = "border-image:url(" + std::string(SCARECROW_IMAGE) + ")";
+    cards["Spring"]    = "border-image:url(" + std::string(SPRING_IMAGE)    + ")";
+    cards["Winter"]    = "border-image:url(" + std::string(WINTER_IMAGE)    + ")";
+    cards["Spy"]       = "border-image:url(" + std::string(SPY_IMAGE)       + ")";
+    cards["Turncoat"]  = "border-image:url(" + std::string(TURNCOAT_IMAGE)  + ")";
+
+    markers["BattleMarker"] = "border-image:url(" + std::string(BATTLE_MARKER_IMAGE)  + ")";
+    markers["FavorMarker"]  = "border-image:url(" + std::string(PEACE_MARKER_IMAGE)   + ")";
+    markers[""]             = "border-image:url(" + std::string(PLAYER_MARKER_IMAGE)  + ")";
+
 }
 
 Campaign::~Campaign()
--- campaign.h
+++ campaign.h
@@ -1,11 +1,11 @@
 #ifndef CAMPAIGN_H
 #define CAMPAIGN_H
 
-<<<<<<< HEAD
+
 #include <QMainWindow>
 #include <QLabel>
 #include <QVector>
-=======
+
 #include <QDialog>
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
@@ -21,7 +21,7 @@
 #include "enumcolor.h"
 #include "purplecard.h"
 #include "player.h"
->>>>>>> adb8dafbb721fdbea13bd93ec3fe388d0a621499
+
 
 
 namespace Ui {
@@ -37,22 +37,19 @@ public:
     ~Campaign();
 
 private:
-<<<<<<< HEAD
+
     Ui::Campaign *ui;
+    Options *options;
+    Help *help;
+    QGraphicsScene *scene = new QGraphicsScene();
+    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    QGraphicsView *view = new QGraphicsView(scene);
+    Game *game;
+    UserInterface *user;
+    std::map<std::string, std::string> cards;
+    std::map<std::string, std::string> markers;
     //QVector<QLabel*> Nameoflabels;
 
-=======
-    Ui::campaign *ui;
-        Options *options;
-        Help *help;
-        QGraphicsScene *scene = new QGraphicsScene();
-        QGraphicsRectItem *rect = new QGraphicsRectItem();
-        QGraphicsView *view = new QGraphicsView(scene);
-        Game *game;
-        UserInterface *user;
-        map<std::string, std::string> cards;
-        map<std::string, std::string> markers;
->>>>>>> adb8dafbb721fdbea13bd93ec3fe388d0a621499
 };
 
 #endif // CAMPAIGN_H
